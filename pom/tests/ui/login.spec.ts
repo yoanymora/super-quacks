dfa75cfcfd0e189e728cedf1e718b6923d17e99f
Francisco Mondragón <fjgm11@hotmail.com>
2026-03-09 12:41:50 -0600

    Updates on Login Test and Login Page

--- pom/tests/ui/login.spec.ts
+++ pom/tests/ui/login.spec.ts
@@ -1,15 +1,19 @@
 import { test, expect } from "@playwright/test";
 import { LoginPage } from "../../pages/login.page";
 import { USER_CREDENTIALS } from "../../data/Constants";
-import { LOGIN_URL } from "../../data/urls";
 
-test.skip("should be able to login correctly", async ({ page }) => {
-	const loginPage = new LoginPage(page);
+test.describe("Login tests", async () => {
+	let loginPage: LoginPage;
 
-	await loginPage.goto(LOGIN_URL);
-	await loginPage.login(
-		USER_CREDENTIALS.STANDARD_USER,
-		USER_CREDENTIALS.PASSWORD
-	);
-	await expect(page.getByRole("link", { name: "Today" })).toBeVisible();
+	test.beforeEach(async ({ page }) => {
+		loginPage = new LoginPage(page);
+	});
+
+	test("should be able to login correctly", async ({ page }) => {
+		await loginPage.login(
+			USER_CREDENTIALS.STANDARD_USER,
+			USER_CREDENTIALS.PASSWORD
+		);
+		await expect(page.getByLabel("Settings")).toBeVisible({ timeout: 50000 });
+	});
 });
