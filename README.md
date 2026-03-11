49eaaa4f16d159414516079a5c5bc1f29cfc07cf
cesar guzman <cesarguzman@cesars-MacBook-Pro.local>
2026-03-11 13:49:55 -0600

    Add Allure report

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # super-quacks
 
 Run command -> npm init playwright@latest
-
-# Mondro was here
+Run command -> npm install
+Intall Allure report:
+Windows: https://allurereport.org/docs/v2/install-for-windows/
+Mac: https://allurereport.org/docs/v2/install-for-macos/
+Linux: https://allurereport.org/docs/v2/install-for-linux/
--- playwright.config.ts
+++ playwright.config.ts
@@ -28,14 +28,19 @@ export default defineConfig({
 	/* Opt out of parallel tests on CI. */
 	workers: process.env.CI ? 1 : undefined,
 	/* Reporter to use. See https://playwright.dev/docs/test-reporters */
-	reporter: "html",
+	reporter: [
+		["line"],
+		["allure-playwright", { outputFolder: "allure-results" }],
+	],
 	/* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
 	use: {
-		/* Base URL to use in actions like `await page.goto('')`. */
-		// baseURL: 'http://localhost:3000',
+		/* Base URL to use in actions like `await page.goto(
+		
 
 		/* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
 		trace: "on-first-retry",
+		screenshot: "only-on-failure",
+		video: "retain-on-failure",
 	},
 
 	/* Configure projects for major browsers */
@@ -45,15 +50,15 @@ export default defineConfig({
 			use: { ...devices["Desktop Chrome"] },
 		},
 
-		{
-			name: "firefox",
-			use: { ...devices["Desktop Firefox"] },
-		},
+		// {
+		// 	name: "firefox",
+		// 	use: { ...devices["Desktop Firefox"] },
+		// },
 
-		{
-			name: "webkit",
-			use: { ...devices["Desktop Safari"] },
-		},
+		// {
+		// 	name: "webkit",
+		// 	use: { ...devices["Desktop Safari"] },
+		// },
 
 		/* Test against mobile viewports. */
 		// {
