ffcb6818301a18637a3011ad8cc90a197eadfabf
Yoany Mora <yoany.stephany@gmail.com>
2026-03-11 17:56:33 -0600

    Remove duplicated function

--- pom/pages/base.page.ts
+++ pom/pages/base.page.ts
@@ -17,21 +17,4 @@ export abstract class BasePage {
 	async addTask(title: string, desc: string) {
 		await this.common.addTask(title, desc);
 	}
-
-	async getTaskId(taskTitle: string) {
-		const response = await this.page.waitForResponse(async (resp) => {
-			if (!resp.url().includes("api/v1/sync") || resp.status() !== 200) {
-				return false;
-			}
-			const body = await resp.json();
-			return body.items && body.items.length > 0;
-		});
-
-		const responseJson = await response.json();
-		const createdTask = responseJson.items.find(
-			(item: { content: string }) => item.content === taskTitle
-		);
-		const taskId = createdTask ? createdTask.id : responseJson.items[0].id;
-		return taskId.toString();
-	}
 }
